6a0aa23dfcc2102bc29adb4c46ea858c80a47e2a
wl18 <robot.me007@gmail.com>
2020-03-16 20:22:37 +0800

    delete myfirstspringboot文件

--- demohello/src/main/java/com/wl/DemohelloApplication.java
+++ demohello/src/main/java/com/wl/DemohelloApplication.java
@@ -1,13 +1,13 @@
-package com.wll.myfirstspringboot;
+package com.wl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyfirstspringbootApplication {
+public class DemohelloApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyfirstspringbootApplication.class, args);
+        SpringApplication.run(DemohelloApplication.class, args);
     }
 
 }
